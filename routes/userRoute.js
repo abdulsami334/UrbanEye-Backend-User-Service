9852b4b57885a94f1d37fa3281695379318d630f
Abdul Sami <samighori921@gmail.com>
2025-07-12 00:49:19 -0700

    Updated backend code

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config(); // ✅ Load environment variables
+
 const multer = require('multer');
 const { storage } = require('../utils/cloudinary');
 const upload = multer({ storage });
- 
-const { signup, Login,adminLogin } = require('../controllers/authcontroller');
 
-router.post('/signup',upload.single('profileImage'), signup);
+const { signup, Login, adminLogin } = require('../controllers/authcontroller');
+
+router.post('/signup', upload.single('profileImage'), signup);
 router.post('/login', Login);
 router.post('/adminlogin', adminLogin);
+
 router.get('/ping', (req, res) => {
   res.send("User API is alive ✅");
 });
 
 module.exports = router;
-
-
